e69980cea4625f4c8b12ec7fd579d523203fdb4c
BharathRoshan <bharathroshan1998@gmail.com>
2021-01-23 11:10:02 +0530

    Created new Components

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+const Square = ({value}) => {
+
+
+    return <button type="button">{ value }</button>;
+}
+
+export default Square
